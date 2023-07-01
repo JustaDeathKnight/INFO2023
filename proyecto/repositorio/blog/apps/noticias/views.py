c50803d881ffb5b8ac2d4b49016de984c9ca3b48
Acevedo Ariel <AcevedoAriel@outlook.com>
2023-06-30 21:52:00 -0300

    clase 29

--- proyecto/repositorio/blog/apps/noticias/views.py
+++ proyecto/repositorio/blog/apps/noticias/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .models import Noticia
 
 # Create your views here.
 
 
 def ListarNoticias(request):
-    return render(request, 'noticias/listar.html')
+    contexto = { }
+    
+    n = Noticia.objects.all() # select * from noticias_noticias
+    
+    contexto['noticias'] = n
+    
+    return render(request, 'noticias/listar.html', contexto)
+
+def DetalleNoticia(request, pk):
+    contexto = { }
+    
+    n = Noticia.objects.get(pk=pk) # select * from noticias_noticias where id = id
+    
+    contexto['noticia'] = n
+    
+    return render(request, 'noticias/detalle.html', contexto)
--- proyecto/repositorio/blog/blog/settings/base.py
+++ proyecto/repositorio/blog/blog/settings/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.usuarios',
+    'apps.noticias',
 ]
 
 MIDDLEWARE = [
@@ -106,6 +107,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(os.path.dirname(BASE_DIR), 'static'),]
 
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(os.path.dirname(BASE_DIR), 'media'),]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
