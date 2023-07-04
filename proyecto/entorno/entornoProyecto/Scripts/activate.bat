181d221f5d99131d2abfffe0158db2027e4f834c
Acevedo Ariel <AcevedoAriel@outlook.com>
2023-07-04 20:11:29 -0300

    Ignorar el entorno

--- proyecto/entorno/entornoProyecto/Scripts/activate.bat
+++ proyecto/entorno/entornoProyecto/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\GitHub\INFO2023\proyecto\entorno\entornoProyecto
+set VIRTUAL_ENV=D:\GitHub\Proyecto-INFO2023\proyecto\entorno\entornoProyecto
 
 if not defined PROMPT set PROMPT=$P$G
 
