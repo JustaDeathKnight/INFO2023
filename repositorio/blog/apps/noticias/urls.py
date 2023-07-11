37f7283cc862e1ba758cdf3111ba286c616783c6
Acevedo Ariel <AcevedoAriel@outlook.com>
2023-07-11 18:26:40 -0300

    crear noticia

--- repositorio/blog/apps/noticias/urls.py
+++ repositorio/blog/apps/noticias/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
 from . import views
 
 app_name = 'noticias'
 
 urlpatterns = [
     path('', views.ListarNoticias, name='listar'),
-    
+
+    path('listarNoticias/', views.ListarNoticias, name='listar'),
+
     path('detalle/<int:pk>/', views.DetalleNoticia, name='detalle'),
-]
+
+    path('addNoticia', views.add_noticia, name='addNoticia')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- repositorio/blog/apps/noticias/views.py
+++ repositorio/blog/apps/noticias/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
-from .models import Noticia
+from django.shortcuts import render, redirect, reverse
+from .models import Noticia, Categoria
+from .forms import NoticiaForm
 
 # Create your views here.
 
 
 def ListarNoticias(request):
-    contexto = { }
+    contexto = { } # diccionario
+    id_categoria = request.GET.get('id', None)
     
-    n = Noticia.objects.all() # select * from noticias_noticias
+    if id_categoria:
+        n = Noticia.objects.filter(categoria_noticia = id_categoria)
+    else:
+        n = Noticia.objects.all() # select * from noticias_noticias
     
     contexto['noticias'] = n
+    
+    cat = Categoria.objects.all().order_by('nombre') # Ordena por nombre
+    contexto['categorias'] = cat
     
     return render(request, 'noticias/listar.html', contexto)
 
@@ -21,3 +29,12 @@ def DetalleNoticia(request, pk):
     contexto['noticia'] = n
     
     return render(request, 'noticias/detalle.html', contexto)
+
+def add_noticia(request):
+    form = NoticiaForm(request.POST or None)
+    
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect(reverse('home'))
+    
+    return render(request, 'noticias/addNoticia.html', {'form': form})
