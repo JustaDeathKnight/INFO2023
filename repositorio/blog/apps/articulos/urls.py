c01ea902d6c0d52a22d77d20f8cb0a876a6141d4
juanma479 <juanmamk9@gmail.com>
2023-07-22 17:15:35 -0300

    agregue la funcion borrar_articulo

--- repositorio/blog/apps/articulos/urls.py
+++ repositorio/blog/apps/articulos/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('categorias/<int:categoria_id>/', views.articulos_por_categoria, name='articulos_por_categoria'),
     path('articulo/<int:articulo_id>/', views.detalle_articulo, name='detalle_articulo'),
     path('add_articulo', views.AddArticulo, name='add_articulo'),
-    
+    path('borrar_articulo/<int:pk>/', views.borrar_articulo, name='borrar_articulo'),
 ]
--- repositorio/blog/apps/articulos/views.py
+++ repositorio/blog/apps/articulos/views.py
@@ -11,6 +11,7 @@ from .forms import ArticuloForm
 def articulos_por_categoria(request, categoria_id):
     categoria = get_object_or_404(Categoria, id = categoria_id)
     articulos = Articulo.objects.filter(categoria_articulo = categoria)
+    
     return render(request,'articulos/articulos_por_categoria.html', {'categoria':categoria, 'articulos': articulos})
 
 def detalle_articulo(request, articulo_id):
@@ -32,3 +33,11 @@ def AddArticulo(request):
     categorias = Categoria.objects.all()
     
     return render(request, 'articulos/add_articulo.html', {'form': form, 'categorias': categorias})
+
+@login_required
+def borrar_articulo(request, pk):
+    articulo = get_object_or_404(Articulo, pk=pk)
+    if request.method == 'POST':
+        articulo.delete()
+        return redirect('home')
+    return render(request, 'articulos/borrar_articulo.html', {'articulo': articulo})
