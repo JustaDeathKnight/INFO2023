bf1195c267b97aa56160635afe9557333a52ebe7
juanma479 <juanmamk9@gmail.com>
2023-07-22 16:49:46 -0300

    agregue el campo autor para los articulos

--- repositorio/blog/apps/articulos/models.py
+++ repositorio/blog/apps/articulos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from apps.usuarios.models import Usuario
 
 # Create your models here.
 
@@ -17,6 +18,7 @@ class Articulo(models.Model):
     fecha_publicacion = models.DateTimeField(auto_now_add=True)
     imagen = models.ImageField(upload_to = 'articulos', default='iconos/default_icon.png')
     categoria_articulo = models.ForeignKey(Categoria, on_delete= models.CASCADE)
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE, default=Usuario.objects.get(is_superuser=True).pk) 
 
     def __str__(self):
         return self.titulo
