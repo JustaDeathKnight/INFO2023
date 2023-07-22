8b06ab6c4329e1d302a105c741c3b52bee3de8b3
juanma479 <juanmamk9@gmail.com>
2023-07-22 16:38:03 -0300

    agregue funcion add_articulo. Tambien en el navbar

--- repositorio/blog/apps/articulos/urls.py
+++ repositorio/blog/apps/articulos/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('categorias/<int:categoria_id>/', views.articulos_por_categoria, name='articulos_por_categoria'),
     path('articulo/<int:articulo_id>/', views.detalle_articulo, name='detalle_articulo'),
+    path('add_articulo', views.AddArticulo, name='add_articulo'),
     
 ]
--- repositorio/blog/apps/articulos/views.py
+++ repositorio/blog/apps/articulos/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Categoria, Articulo
+from django.contrib.auth.decorators import login_required
+from apps.usuarios.models import Usuario
+from .forms import ArticuloForm
+
 
 # Create your views here.
 
@@ -12,3 +16,19 @@ def articulos_por_categoria(request, categoria_id):
 def detalle_articulo(request, articulo_id):
     articulo = get_object_or_404(Articulo, id=articulo_id)
     return render(request, 'articulos/detalle_articulo.html', {'articulo': articulo})
+
+@login_required
+def AddArticulo(request):
+    if request.method == 'POST':
+        form = ArticuloForm(request.POST, request.FILES) ##REQUEST FILE PARA LAS IMAGENES
+        if form.is_valid():
+            articulo = form.save(commit=False)
+            articulo.author = request.user #autor de la noticia
+            articulo.save()
+            return redirect('home')
+    else:
+        form = ArticuloForm()
+    
+    categorias = Categoria.objects.all()
+    
+    return render(request, 'articulos/add_articulo.html', {'form': form, 'categorias': categorias})
