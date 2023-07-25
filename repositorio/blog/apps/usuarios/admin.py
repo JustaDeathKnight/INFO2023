c41bd0929897f4e4c953ff99a65ffd12f2f7a980
Acevedo Ariel <AcevedoAriel@outlook.com>
2023-07-25 19:08:15 -0300

    Crear categoria tile de creacion

--- repositorio/blog/apps/usuarios/admin.py
+++ repositorio/blog/apps/usuarios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from .models import Usuario
 
 # Register your models here.
+admin.site.register(Usuario)
--- repositorio/blog/apps/usuarios/models.py
+++ repositorio/blog/apps/usuarios/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Usuario(AbstractUser):
-    imagen = models.ImageField(upload_to='usuarios', default='user/user_avatar_profile_icon.png')
+    imagen = models.ImageField(upload_to='usuarios', default='usuarios/user_avatar_profile_icon.png')
     is_colaborador = models.BooleanField(default=False)
     pass
