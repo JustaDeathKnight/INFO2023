be2b7ad5b93c9b995227481083753ae69cf36eb0
Acevedo Ariel <AcevedoAriel@outlook.com>
2023-07-29 15:05:14 -0300

    fix y template para cambio de imagen de usuario

--- repositorio/blog/apps/usuarios/models.py
+++ repositorio/blog/apps/usuarios/models.py
@@ -5,4 +5,3 @@ from django.contrib.auth.models import AbstractUser
 class Usuario(AbstractUser):
     imagen = models.ImageField(upload_to='usuarios', default='usuarios/user_avatar_profile_icon.png')
     is_colaborador = models.BooleanField(default=False)
-    pass
