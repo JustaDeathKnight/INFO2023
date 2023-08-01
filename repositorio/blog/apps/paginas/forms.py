d48be32a490a7bcb118c4415f503209a7e15784d
Acevedo Ariel <AcevedoAriel@outlook.com>
2023-08-01 18:41:28 -0300

    Revert "Develop_juanma"

--- repositorio/blog/apps/paginas/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class FormularioContacto(forms.Form):
-    nombre = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    mensaje = forms.CharField(widget=forms.Textarea)
