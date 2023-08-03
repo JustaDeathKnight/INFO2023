b8559f0d7030285f946f1b11160d664d28262a3f
Acevedo Ariel <AcevedoAriel@outlook.com>
2023-08-03 00:19:51 -0300

    fix default user articulos

--- repositorio/blog/apps/articulos/models.py
+++ repositorio/blog/apps/articulos/models.py
@@ -33,8 +33,8 @@ class Articulo(models.Model):
     imagen = models.ImageField(upload_to = 'articulos', default='iconos/default_icon.png')
     categoria_articulo = models.ForeignKey(Categoria, on_delete= models.CASCADE)
     # Para una BD nueva se debe crear sin default, sino provoca error al migrar
-    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    # autor = models.ForeignKey(Usuario, on_delete=models.CASCADE, default=Usuario.objects.filter(is_superuser=True).first().pk) 
+    # autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE, default=Usuario.objects.filter(is_superuser=True).first().pk) 
     comentarios = models.ManyToManyField(Comentario, related_name='articulo')
 
     def __str__(self):
