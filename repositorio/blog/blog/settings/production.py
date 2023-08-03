aa5e17d920acaf92184bbb827ccfc274d046c824
Acevedo Ariel <AcevedoAriel@outlook.com>
2023-08-03 00:04:46 -0300

    fox DB production

--- repositorio/blog/blog/settings/production.py
+++ repositorio/blog/blog/settings/production.py
@@ -15,7 +15,7 @@ DATABASES = {
     'default': {
         # Motor de la base de datos
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'JaDK$default',
+        'NAME': 'JaDK$blog_cocina',
         # Credenciales de acceso
         'USER': 'JaDK',
         'PASSWORD': 'informatorio2023',
